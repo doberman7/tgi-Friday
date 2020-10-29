cceacb4de951fb89d7b8433923d3f3d1e2040b73
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 13:30:50 -0600

    edited to debug 2

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -1,54 +1,79 @@
 log = console.log
 //return the number of times a month ends with a Friday.
 
-let initialYear = 1991// it ("returns 1 when 1991"
+let initialYear = 2017 // Not the correct number of Fridays in 2017 - Expected: 2, instead got: 1
 let endYear = undefined
 
 
 
-
-
 function lastDayIsFriday(initialYear) {
-    let count = 0
-    let lastday = (y, m) =>new Date(y, m , 0).getDay()
-    let dayNum = null
-    for ( mes = 1; mes < 13; mes++) {
-       dayNum = lastday(initialYear, mes)
-      // console.log(mes);
-    }
+  let count = 0
+  let date = null
+  let dayNum = null
+  let dateObjOfMonth = null
+  //funcionio q obtiene el ulttimo dia en un objDate
+  let lastday = (y, m) => {
+    date = new Date(y, m, 0)
+    return date
+  }
 
-    switch (dayNum) {
-      case 1:
-        dayNum = "Monday"
-        count ++
-        break;
-      default:
-      case 2:
-        dayNum ="Tuesday"
-        count ++
-        break;
-      case 3:
-        dayNum ="Wednesday"
-        count ++
-        break;
-      case 4:
-        dayNum ="Thursday"
-        count ++
-        break;
-      case 5:
-        dayNum ="Friday"
-        count ++
-        break;
-      case 6:
-        dayNum ="Saturday"
-        count ++
-        break;
+  for (mes = 1; mes < 13; mes++) {
+    dateObjOfMonth = lastday(initialYear, mes)
+    console.log(dateObjOfMonth);
+    dayNum = dateObjOfMonth.getDay()
+  }
 
-    }
+  switch (dayNum) {
+    case 1:
+      // console.log(dateObjOfMonth);
+      dayNum = "Monday"
+      log(dayNum)
+
+      count++
+      break;
+    default:
+    case 2:
+      // console.log(dateObjOfMonth);
+      dayNum = "Tuesday"
+      log(dayNum)
+
+      count++
+      break;
+    case 3:
+      // console.log(dateObjOfMonth);
+      dayNum = "Wednesday"
+      log(dayNum)
+
+      count++
+      break;
+    case 4:
+      // console.log(dateObjOfMonth);
+      dayNum = "Thursday"
+      log(dayNum)
+
+      count++
+      break;
+    case 5:
+      // console.log(dateObjOfMonth);
+      dayNum = "Friday"
+      log(dayNum)
+      count++
+      break;
+    case 6:
+      // console.log(dateObjOfMonth);
+      dayNum = "Saturday"
+      log(dayNum)
+
+
+      count++
+      break;
+
+  }
 
 
 
-    console.log(count);
+  console.log("count", count)
+  return count
 }
 
 
