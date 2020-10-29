0fbd4800aba68a51d1c87592e0f9638dc541039a
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 11:44:00 -0600

    get last day of month

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -1,7 +1,8 @@
+//return the number of times a month ends with a Friday.
 log = console.log
 var lastday = function(y, m) {
   return new Date(y, m + 1, 0).getDate();
 }
-console.log(lastday(2014, 0));
-console.log(lastday(2014, 1));
-console.log(lastday(2014, 11));
+console.log(lastday(2020, 10));
+// console.log(lastday(2014, 1));
+// console.log(lastday(2014, 11));
