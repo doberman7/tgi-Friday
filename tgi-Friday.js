c4e51e87cde8dad600caa9b6a7c358ad003620ff
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 17:23:34 -0600

    closer

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -18,23 +18,40 @@ function lastDayIsFriday(initialYear, endYear) {
     return inOnYear
   } else {
 
-    let severalYears = moreThatOneYear()
+    let severalYears = fridaysInSeveralYears()
 
     // console.log(severalYears)
     // return severalYears
   }
 
-  function moreThatOneYear() {
+  function fridaysInSeveralYears() {
     let sum = 0
     //determinar años entre initialYear y endYear
     let years = endYear - initialYear
-    // console.log(years);
-    //correr fridaysInAyear ese numero
+
+    //durante el numero años
     for ( i = 1; i <= years; i++) {
-      // console.log("----------index",i)
-      sum += fridaysInAyear()
-      console.log("met",sum)
+      //funcion q obtiene el ulttimo dia en un objDate
+      let lastDay = (y, m) => {
+        date = new Date(y, m, 0)
+        return date
+      }
+      //iterara del 1 al 12
+      for (mes = 1; mes < 13; mes++) {
+        //obtener ulttimo día de cada mes
+        dateObjOfMonth = lastDay(initialYear, mes)
+        // obtener numero del 0 al 6 q refleja días de domingo a lunes
+        dayNum = dateObjOfMonth.getDay()
+        //  si el numero es 5 es viernes y aumentar conteoo
+        if (dayNum == 5) {
+          console.log(dateObjOfMonth);
+          dayNum = "Friday"
+          sum++
+        }
+
+      }
     }
+    console.log(sum);
     return sum
   }
 
