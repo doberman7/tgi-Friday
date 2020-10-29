abd2293e4e3de440eb9060e5992b44e8713d8514
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 13:58:23 -0600

    encapsuled

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -2,32 +2,46 @@ log = console.log
 //return the number of times a month ends with a Friday.
 
 let initialYear = 2017 //2017 - Expected: 2, instead got: 1
-let endYear = undefined
+// let endYear = undefined
 
 
 
-function lastDayIsFriday(initialYear) {
+function lastDayIsFriday(initialYear, endYear) {
   let count = 0
   let date = null
   let dayNum = null
   let dateObjOfMonth = null
-  //funcion q obtiene el ulttimo dia en un objDate
-  let lastday = (y, m) => {
-    date = new Date(y, m, 0)
-    return date
+
+  if (typeof endYear == "undefined") {
+    onlyOneYear()
+  } else {
+
   }
-//iterara del 1 al 12
-  for (mes = 1; mes < 13; mes++) {
-    //obtener ulttimo día de cada mes
-    dateObjOfMonth = lastday(initialYear, mes)
-    // obtener numero del 0 al 6 q refleja días de domingo a lunes
-    dayNum = dateObjOfMonth.getDay()
-    //  si el numero es 5 es viernes y aumentar conteoo
-    if (dayNum == 5) {
-      dayNum = "Friday"
-      count++
+
+  function onlyOneYear() {
+    //funcion q obtiene el ulttimo dia en un objDate
+    let lastday = (y, m) => {
+      date = new Date(y, m, 0)
+      return date
+    }
+    //iterara del 1 al 12
+    for (mes = 1; mes < 13; mes++) {
+      //obtener ulttimo día de cada mes
+      dateObjOfMonth = lastday(initialYear, mes)
+      // obtener numero del 0 al 6 q refleja días de domingo a lunes
+      dayNum = dateObjOfMonth.getDay()
+      //  si el numero es 5 es viernes y aumentar conteoo
+      if (dayNum == 5) {
+        dayNum = "Friday"
+        count++
+      }
     }
   }
+
+
+
+
+
   console.log(count)
   return count
 }
