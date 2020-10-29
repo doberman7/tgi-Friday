a0d7f17c4e416325e8251a24b702d63bb93d907b
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 12:43:41 -0600

    added count

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -9,30 +9,36 @@ let endYear = undefined
 
 
 function lastDayIsFriday(initialYear) {
-
+    let count = 0
     let lastday = (y, m) =>new Date(y, m , 0).getDay()
 
-    let dayNum = lastday(initialYear, 11)
+    let dayNum = lastday(initialYear, 10)
 
     switch (dayNum) {
       case 1:
         dayNum = "Monday"
+        count ++
         break;
       default:
       case 2:
         dayNum ="Tuesday"
+        count ++
         break;
       case 3:
         dayNum ="Wednesday"
+        count ++
         break;
       case 4:
         dayNum ="Thursday"
+        count ++
         break;
       case 5:
         dayNum ="Friday"
+        count ++
         break;
       case 6:
         dayNum ="Saturday"
+        count ++
         break;
 
     }
