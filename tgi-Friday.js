86dd8b999f98d1abe8b41af2bf238f9095aff7a7
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 12:31:16 -0600

    get rigth day

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -11,13 +11,37 @@ let endYear = undefined
 function lastDayIsFriday(initialYear) {
 
     let lastday = function(y, m) {
-      return new Date(y, m + 1, 0).getDate();
+      //getDay() -> 0 for Sunday, 1 for Monday, 2 for Tuesday, 3 for Wednesday, 4 for Thursday, 5 for Friday, 6 for Saturday.
+      let date = new Date(y, m , 0);
+      // log(date.getDay())
+      return date.getDay()
+    }
+    let dayNum = lastday(initialYear, 10)
+
+    switch (dayNum) {
+      case 1:
+        dayNum = "Monday"
+        break;
+      default:
+      case 2:
+        dayNum ="Tuesday"
+        break;
+      case 3:
+        dayNum ="Wednesday"
+        break;
+      case 4:
+        dayNum ="Thursday"
+        break;
+      case 5:
+        dayNum ="Friday"
+        break;
+      case 6:
+        dayNum ="Saturday"
+        break;
+
     }
 
-
-
-
-    console.log(lastday(initialYear, 10));
+    console.log(dayNum);
 }
 
 
