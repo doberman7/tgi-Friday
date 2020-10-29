4873b2e1e2737205cd51442a1ba1d6ffa9e3be3f
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-29 12:01:32 -0600

    refactoriing

--- tgi-Friday.js
+++ tgi-Friday.js
@@ -1,8 +1,24 @@
-//return the number of times a month ends with a Friday.
 log = console.log
-var lastday = function(y, m) {
-  return new Date(y, m + 1, 0).getDate();
+//return the number of times a month ends with a Friday.
+
+let initialYear = 2020// it ("returns 1 when 1991"
+let endYear = undefined
+
+
+
+
+
+function lastDayIsFriday(initialYear) {
+
+    let lastday = function(y, m) {
+      return new Date(y, m + 1, 0).getDate();
+    }
+
+
+
+
+    console.log(lastday(initialYear, 10));
 }
-console.log(lastday(2020, 10));
-// console.log(lastday(2014, 1));
-// console.log(lastday(2014, 11));
+
+
+lastDayIsFriday(initialYear)
